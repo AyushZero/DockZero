f27de493f580e3b3bfe0a84fc8fa6adaea08d60e
Ayush Singh <ayushsingh30082005@gmail.com>
2025-05-04 16:05:19 +0530

    Added icons.

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,10 +9,5 @@ namespace DockZero;
 /// </summary>
 public partial class App : Application
 {
-    protected override void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
-        IconConverter.ConvertPngsToIco();
-    }
 }
 
